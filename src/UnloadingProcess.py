aefab50d21a249e3fdadb95a956b3a3ba6bf3e9d
Kaeden Lewis <lewiski@appstate.edu>
2025-02-18 15:55:53 -0500

    Added UnloaderList and updated necessary files, along with data

--- src/UnloadingProcess.py
+++ src/UnloadingProcess.py
@@ -8,6 +8,7 @@ def unloading(env, unloader, trucks):
         time_taken = truck.size / unloader.pph
         for each_truck in trucks:
             each_truck[1].time += time_taken
+            print(f"each_truck[1].time: {each_truck[1].time}")
         yield req
 
         print('The unloader ' + str(unloader.eid) + ' is unloading truck ' + str(truck.po) + ' at ' + str(env.now))
