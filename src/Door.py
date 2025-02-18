eb9206e23c20d53e699ec72bdc2cbd07f1c0bf92
Harlee <mintonhs@appstate.edu>
2025-02-18 15:06:29 -0500

    - Implement method for assigning unloader and truck to a door object
    - Implement method for filling dockspace infront of door with the pallets from truck

--- src/Door.py
+++ src/Door.py
@@ -3,4 +3,10 @@ class Door:
         self.number = number
         self.truck_and_unloader = truck_and_unloader
         self.pallets = pallets
+    
+    def assign_job(self, truck, unloader):
+        self.truck_and_unloader = (truck, unloader)
+    
+    def fill_dock(self):
+        self.pallets = self.truck_and_unloader[0].size
 
