22a33d108655bb98aa24c91b03da05917b0e0170
Kaeden Lewis <lewiski@appstate.edu>
2025-02-23 21:55:21 -0500

    Code organization and documentation

--- src/Main.py
+++ src/Main.py
@@ -7,38 +7,39 @@ from TruckList import TruckList
 import csv_reader
 from UnloaderList import UnloaderList
 
-
+"""
+Main method to run all code. Currently makes the truck and employee
+list, initializes the enviorment, and prints enviorment time.
+"""
 def __main__():
-    
-
-
     trucks = TruckList()
-
     start_truck = trucks.removeTruck()
-
     env = Environment()
-
     trucks.addTruck(start_truck)
-
-
     unloaders = UnloaderList(env)
-
     env.process(process_generator(env, trucks, unloaders.list))
-
     
     print('The current time is: ' + str(env.now))
     env.run()
-    
     print('The current time is: ' + str(env.now))
 
+"""
+Method to process the trucks with the unloaders, assigining the trucks to the unloaders.
+
+Args:
+    env (Environment): enviorment which is used to process.
+    trucks (list): list of trucks that need to be unloaded.
+    unloaders (list): unloaders that will unload the trucks.
+"""
 def process_generator(env, trucks, unloaders):
     i = 0
-
     for t in trucks:
         yield env.timeout(t[1].time)
-
         env.process(unloading(env, unloader=unloaders[i % 3], trucks=trucks))
         i += 1
 
+"""
+Run the code.
+"""
 if __name__ == __main__():
     __main__()
--- src/Truck.py
+++ src/Truck.py
@@ -1,13 +1,42 @@
-
+"""
+Truck class that contains the truck object constructor,
+the incrementTime method, and __lt__ method.
+"""
 class Truck(object):
+    """
+    Constructor to create a truck object.
+
+    Args:
+        po(int): priority number of the truck, determines how
+                 quickly an unloader will get around to it.
+        size(int): size of the truck, determines how long it 
+                   takes to unload
+        live(int): Determines if the truck is a live load or not
+        time(int): Determines the time of the truck's arrival.
+    """
     def __init__(self, po, size, live, time):
         self.po = po
         self.size = size
         self.live = live
         self.time = time
 
+    """
+    Increments the time of a truck by one, simulating the
+    movement of time.
+    """
     def incrementTime(self):
         self.time += 1
 
+    """
+    Checks if another truck's size and time of arrival is greater than its own.
+    In the case of time, it checks if its greater than or equal to.
+
+    Args:
+        other(Truck): Another truck to be compared to.
+
+    Returns:
+        True or false based on if the current truck has a smaller size and
+        earlier arrival time.
+    """
     def __lt__(self, other):
         return self.size < other.size and self.time <= other.time
